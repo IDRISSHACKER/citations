9b107e51d7ea05c2eb42ea4281d0cec90934b429
IDRISSHACKER <idrisscoder@gmail.com>
2022-02-21 00:52:20 +0100

    Verification de la taille des citations

--- scripts/app.js
+++ scripts/app.js
@@ -1,33 +1,39 @@
 import { citations } from "./citations.js";
 
+const DERNIERR_CITATION_STR = "dernierreCitation"
+const DERNIERR_JOUR_STR     = "dernierJour"
+const INITIAL_CITATION      = 0
+
 window.onload = ()=>{
-  let citation = document.querySelector(".citation");
+let citation = document.querySelector(".citation");
 let auteur = document.querySelector(".auteur");
 let date = document.querySelector(".date");
 
 const day = new Date()
-const currentDay = day.toLocaleString().split(",")[0]
+const currentDay = day.toLocaleString().split(",")[INITIAL_CITATION]
 
-let dernierreCitation = 0
+let dernierreCitation = INITIAL_CITATION
 let dernierJour = ""
 
-if(localStorage.getItem("dernierreCitation") && localStorage.getItem("dernierJour")){
-  dernierreCitation = parseInt(localStorage.getItem("dernierreCitation"))
-  dernierJour = localStorage.getItem("dernierJour")
+if(localStorage.getItem(DERNIERR_CITATION_STR) && localStorage.getItem(DERNIERR_JOUR_STR)){
+  dernierreCitation = parseInt(localStorage.getItem(DERNIERR_CITATION_STR))
+  dernierJour = localStorage.getItem(DERNIERR_JOUR_STR)
   if (dernierJour !== currentDay) {
     dernierreCitation = dernierreCitation + 1
-    localStorage.setItem("dernierreCitation", dernierreCitation)
-    localStorage.setItem("dernierJour", currentDay)
+    if (dernierreCitation <= citations.length) {
+      localStorage.setItem(DERNIERR_CITATION_STR, dernierreCitation)
+    }else {
+      dernierreCitation = INITIAL_CITATION
+      localStorage.setItem(DERNIERR_CITATION_STR, INITIAL_CITATION)
+    }
+    localStorage.setItem(DERNIERR_JOUR_STR, currentDay)
   }
 } else {
-  localStorage.setItem("dernierreCitation", 0)
-  localStorage.setItem("dernierJour", currentDay)
+  localStorage.setItem(DERNIERR_CITATION_STR, INITIAL_CITATION)
+  localStorage.setItem(DERNIERR_JOUR_STR, currentDay)
 }
 
 let dateValue = new Date();
-
-
-
 
 citation.textContent = citations[dernierreCitation][0];
 auteur.textContent = citations[dernierreCitation][1];
