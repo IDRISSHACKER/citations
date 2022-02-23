5f865eafd8629dbf2eb39c6b58cfa4fb47cf91d7
idriss-coder <idrisscoder@gmail.com>
2022-02-23 23:44:10 +0100

    Rectification de la fonctionnalitée permettant de changer une citation automatiquement avec la fonction localstorage qui permert de sauvegarder les datas dans le navigateur de l'utilisateur

--- scripts/app.js
+++ scripts/app.js
@@ -1,28 +1,47 @@
 import { citations } from "./citations.js";
 
-let citation = document.querySelector(".citation");
+window.onload = ()=>{
+  let citation = document.querySelector(".citation");
 let auteur = document.querySelector(".auteur");
 let date = document.querySelector(".date");
 
+const day = new Date()
+const currentDay = day.toLocaleString().split(",")[0]
+
+let dernierreCitation = 0
+let dernierJour = ""
+
+if(localStorage.getItem("dernierreCitation") && localStorage.getItem("dernierJour")){
+  dernierreCitation = parseInt(localStorage.getItem("dernierreCitation"))
+  dernierJour = localStorage.getItem("dernierJour")
+  if (dernierJour !== currentDay) {
+    dernierreCitation = dernierreCitation + 1
+    localStorage.setItem("dernierreCitation", dernierreCitation)
+    localStorage.setItem("dernierJour", currentDay)
+  }
+} else {
+  localStorage.setItem("dernierreCitation", 0)
+  localStorage.setItem("dernierJour", currentDay)
+}
+
 let dateValue = new Date();
 
-let time = 86400000;
-let count = 0;
 
-setInterval(() => {
-  count = count + 1;
-  citation.textContent = citations[count][0];
-  auteur.textContent = citations[count][1];
 
-  dateValue = new Date();
 
-  let dateLocale = dateValue.toLocaleString("fr-FR", {
-    weekday: "long",
-    year: "numeric",
-    month: "long",
-    day: "numeric",
-  });
+citation.textContent = citations[dernierreCitation][0];
+auteur.textContent = citations[dernierreCitation][1];
 
-  date.innerHTML = dateLocale;
+dateValue = new Date();
 
-}, time);
+let dateLocale = dateValue.toLocaleString("fr-FR", {
+  weekday: "long",
+  year: "numeric",
+  month: "long",
+  day: "numeric",
+});
+
+date.innerHTML = dateLocale;
+}
+
+
